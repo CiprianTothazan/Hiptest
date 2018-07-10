2b4bc1ecf4df1252bc6e82de201b5fed48509b69
CiprianTothazan <41068156+CiprianTothazan@users.noreply.github.com>
2018-07-10 16:42:15 +0200

    Add files via upload

---
+++ actionwords.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+
+module Actionwords
+  def should_return_the_average
+
+  end
+
+  def should_return_the_maximum_number
+
+  end
+
+  def should_return_the_minimum_number
+
+  end
+end
